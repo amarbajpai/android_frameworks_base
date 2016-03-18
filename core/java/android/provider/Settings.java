47e4d928f0c36c107c083d1918b6a4b7d5c4ea9e
maxwen <max.weninger@gmail.com>
2016-03-18 18:17:05 +0100

    Volume key music control [1/2]
    Change-Id: Ib0839d4fb7f411d85023aa1de61716b7f78f2e97
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4306,6 +4306,13 @@ public final class Settings {
         public static final String QS_TILE_STYLE = "qs_tile_style";
 
         /**
+         * Control music seeking with volume up/down buttons
+         *
+         * @hide
+         */
+        public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
