e08707946cb887975e2280b04dac5e960380b306
Alex Cruz <alex@dirtyunicorns.com>
2018-10-31 18:01:14 -0500

    Change QS tiles styles / refactor a little
    We're going to introduce a more AOSP-like set of styles, so we
    need to change the overlay package names.
    
    This also aligns this with our accent picker in that we don't need
    to include a 'stock' overlay because for the default package you're
    just unloading the overlays and so reverts back to stock.
    
    Change-Id: If8f791b9fdb6b07caf83e9899c9b6ebfd4e0b864
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4104,9 +4104,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         ThemeAccentUtils.updateTileStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), qsTileStyle);
     }
 
-    // Unload all qs tile styles back to stock
-    public void stockTileStyle() {
-        ThemeAccentUtils.stockTileStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
+    // Unload all the qs tile styles
+    public void unlockQsTileStyles() {
+        ThemeAccentUtils.unlockQsTileStyles(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
     private void updateDozingState() {
@@ -5313,7 +5313,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 updateKeyguardStatusSettings();
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.QS_TILE_STYLE))) {
-                stockTileStyle();
+                unlockQsTileStyles();
                 updateTileStyle();
             }
         }
